866bfb3365107d1fc9377b62c7c019efff829eea
이강민 <lee.kangmin@postype.com>
2023-02-27 15:44:05 +0900

    BB-0015 fix: app.module.ts 수정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { DynamoDBService } from 'src/dynamodb/dynamodb.service';
 import { PostsModule } from 'src/posts/posts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AwsBlogCrawler } from '../crawlers/awsBlogCrawler'; // Import the AwsBlogCrawler
 import { GoogleBlogCrawler } from '../crawlers/googleBlogCrawler';
 import { CronService } from '../cron/cron.service';
 
 @Module({
   imports: [PostsModule],
   controllers: [AppController],
-  providers: [AppService, DynamoDBService, CronService, GoogleBlogCrawler],
+  providers: [
+    AppService,
+    DynamoDBService,
+    CronService,
+    GoogleBlogCrawler,
+    AwsBlogCrawler,
+  ],
 })
 export class AppModule {}
